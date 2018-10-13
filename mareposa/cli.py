d89b3ef4532834bbe9992e79a27693d434e1b242
RichStone <richard.had@hotmail.de>
2018-10-13 16:52:20 +0200

    add remote repo creation functionality

--- mareposa/cli.py
+++ mareposa/cli.py
@@ -4,7 +4,7 @@ import click
 
 @click.group()
 def main():
-    """Fla maREPOsa allows you to go through all the initiation process of new git/GitHub projects with just a single terminal command."""
+    """la maREPOsa allows you to go through all the initiation process of new git/GitHub projects with just a single terminal command."""
     pass
 
 
@@ -14,19 +14,33 @@ def mareposa():
 
 
 @mareposa.command()
-@click.option('-l', '--locally', is_flag=True, help='Create a git repository locally and commit all existing files.')
-def create(locally):
-    if locally is False:
-        click.echo('Please decide if you want to create a local repository and commit your files.')
+@click.option('-l', '--locally', is_flag=True, help='Initiate a git repository locally and commit all existing files of the current directory.')
+@click.option('-g', '--github-repo', is_flag=True,
+              help='Create a remote repository on GitHub. '
+                   'To create a new remote GitHub repo, please provide your registered --gh-user (GitHub username) and your new --repo-name')
+@click.option('--gh-user', default=None, help='User name of the owner of the new ')
+@click.option('--repo-name', help='Your new repository name.')
+def create(locally, github_repo, gh_user, repo_name):
+    if locally is False and github_repo is False:
+        click.echo('Create new repository locally, remotely or both? For more information type `mareposa create --help`')
     else:
-        # bash_execute('git init', 'git add .', 'git commit -m\"start project\"')
-        bash_execute(['git', 'init'], ['git', 'add', '.'], ['git', 'commit', '-m"start project"'])
+        # if locally:
+        #     bash_execute(['git', 'init'], ['git', 'add', '.'], ['git', 'commit', '-m"start project"'])
+        if github_repo:
+            if gh_user and repo_name:
+                bash_execute(['curl', '-u', gh_user, 'https://api.github.com/user/repos', '-d', '{"name":"' + repo_name + '"}'])
+            else:
+                click.echo('Sorry, some parameters are missing.\n '
+                           'Please provide all of these options when you plan to create a new remote GitHub repository:\n '
+                           '--gh-user\n '
+                           '--repo-name')
 
 
 def bash_execute(*commands):
     for command in commands:
         process = subprocess.Popen(command, stdout=subprocess.PIPE)
         output, error = process.communicate()
+        click.echo(output)
         process.terminate()
 
 
